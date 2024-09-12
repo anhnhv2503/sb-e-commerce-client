3c9e4149bdbb4e70feaccfbed5e32c7dcb5aaad9
Nguyen Ha Viet Anh <nguyenhavietanh2503@gmail.com>
2024-09-12 14:12:07 +0700

    Initial project for client

---
+++ src/components/service/axiosConfig.jsx
@@ -0,0 +1,71 @@
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080",
+});
+
+axios.interceptors.request.use(
+  function (config) {
+    // Làm gì đó trước khi request dược gửi đi
+    return config;
+  },
+  function (error) {
+    // Làm gì đó với lỗi request
+    return Promise.reject(error);
+  }
+);
+
+// Thêm một bộ đón chặn response
+axios.interceptors.response.use(
+  function (response) {
+    // Bất kì mã trạng thái nào nằm trong tầm 2xx đều khiến hàm này được trigger
+    // Làm gì đó với dữ liệu response
+    return response;
+  },
+  function (error) {
+    // Bất kì mã trạng thái nào lọt ra ngoài tầm 2xx đều khiến hàm này được trigger\
+    // Làm gì đó với lỗi response
+    const status = error.response?.status || 500;
+    // we can handle global errors here
+    switch (status) {
+      // authentication (token related issues)
+      case 401: {
+        toast.error("Unauthorized");
+        return Promise.reject(error);
+      }
+
+      // forbidden (permission related issues)
+      case 403: {
+        return Promise.reject(error);
+      }
+
+      // bad request
+      case 400: {
+        return Promise.reject(error);
+      }
+
+      // not found
+      case 404: {
+        return Promise.reject(error);
+      }
+
+      // conflict
+      case 409: {
+        return Promise.reject(error);
+      }
+
+      // unprocessable
+      case 422: {
+        return Promise.reject(error);
+      }
+
+      // generic api error (server related) unexpected
+      default: {
+        return Promise.reject(error);
+      }
+    }
+  }
+);
+
+export default instance;
