562dce2bb24cefecd17c8a7855e19fbdc6ae2648
anhnhv2503 <nguyenhavietanh2503@gmail.com>
2024-09-12 23:02:48 +0700

    final commit of day 1

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -236,12 +236,12 @@ const Header = () => {
                 </a>
               </div>
               <div className="py-6">
-                <a
-                  href="/login"
+                <Link
+                  to="/login"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Log in
-                </a>
+                </Link>
               </div>
             </div>
           </div>
